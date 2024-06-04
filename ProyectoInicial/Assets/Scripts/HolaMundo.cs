abeb7fb0eeb72c4ba36640f385d7f6f11a577d66
cagf84 <cagf.02@gmail.com>
2024-06-03 22:50:18 -0400

    Update HolaMundo.cs
    Clase-Modulo-5

--- ProyectoInicial/Assets/Scripts/HolaMundo.cs
+++ ProyectoInicial/Assets/Scripts/HolaMundo.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+    int x;
     // Start is called before the first frame update
     void Start()
     {
-        print("Algo a mostrar");
-        Debug.Log("Algo paso");
-        Debug.LogWarning("Algo salio con alerta");
-        Debug.LogError("Algo salio con Error");
+        x = 0;
+        //print("Algo a mostrar");     
+        
+        //Debug.LogWarning("Algo salio con alerta");
+        //Debug.LogError("Algo salio con Error");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x = x + 1;
+        Debug.Log(x);
+        //Debug.Log("Algo paso");
     }
 }
